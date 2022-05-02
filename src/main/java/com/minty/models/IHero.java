fbf2f11cf51ca99f7cc32c69043262367e75c5c4
bryanbill <emobrya@gmail.com>
2022-05-02 09:19:19 +0300

    fix:Models - Fixed Model Structure

--- src/main/java/com/minty/models/IHero.java
+++
@@ -1,5 +0,0 @@
-package com.minty.models;
-
-public interface IHero {
-   public boolean addToSquad(Squad squad);
-}
--- src/main/java/com/minty/models/ISquad.java
+++
@@ -1,6 +0,0 @@
-package com.minty.models;
-
-public interface ISquad {
-     boolean addToSquad(int squadId, Hero hero);
-     boolean removeFromSquad(int squadId, Hero hero);
-}
