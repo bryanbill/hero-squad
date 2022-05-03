75d75119ff3d578f6ed943591683c7157455f231
bryanbill <emobrya@gmail.com>
2022-05-03 16:27:07 +0300

    ft:Js - Js Utility function for Delete Requests

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,1 +1,12 @@
-console.log("And We are up and running");
+function delReq(uri) {
+    fetch(uri, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(e => {
+        window.alert("Deleted");
+    }).catch(e => {
+        window.alert("Error");
+    });
+}
